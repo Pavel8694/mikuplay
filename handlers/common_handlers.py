b1df76359a8835576562ddfe09db478a2fd04196
livingsamurai <33865802+Pavel8694@users.noreply.github.com>
2024-12-03 21:50:53 +0900

    alpha 0.1.7 fix 2

--- handlers/common_handlers.py
+++ handlers/common_handlers.py
@@ -4,17 +4,21 @@ from aiogram.fsm.context import FSMContext
 from sqlalchemy.ext.asyncio import AsyncSession
 from handlers.admin_handlers import show_admin_menu, is_admin
 from handlers.user_handlers import show_user_menu
+import logging
 
 common_router = Router()
+logger = logging.getLogger(__name__)
 
 @common_router.callback_query(F.data == "cancel_action")
 async def cancel_action(callback_query: CallbackQuery, state: FSMContext, session: AsyncSession):
     if await state.get_state() is None:
         await callback_query.answer("⚠️ Действие уже отменено.", show_alert=True)
+        logger.info(f"⚠️ Пользователь @{callback_query.from_user.username} ({callback_query.from_user.id}) попытался отменить неактивное действие.")
         return
 
     await state.clear()
     await callback_query.answer("✅ Действие отменено.")
+    logger.info(f"✅ Пользователь @{callback_query.from_user.username} ({callback_query.from_user.id}) отменил действие и вернулся в главное меню.")
     
     # Проверяем, является ли пользователь администратором и показываем соответствующее меню
     if await is_admin(callback_query.from_user.id, session):
@@ -26,6 +30,7 @@ async def cancel_action(callback_query: CallbackQuery, state: FSMContext, sessio
 async def back_to_menu(callback_query: CallbackQuery, state: FSMContext, session: AsyncSession):
     await state.clear()
     await callback_query.answer("🔁 Возвращаемся в главное меню.")
+    logger.info(f"🔁 Пользователь @{callback_query.from_user.username} ({callback_query.from_user.id}) вернулся в главное меню.")
     
     # Проверяем, является ли пользователь администратором и показываем соответствующее меню
     if await is_admin(callback_query.from_user.id, session):
